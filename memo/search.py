60e2f460521f7f27f4373205f519327f4721a910
Sean Choi <seanchoi@seans-mac-mini.myfiosgateway.com>
2020-10-25 03:43:22 -0400

    phone optimized

--- memo/search.py
+++ memo/search.py
@@ -8,14 +8,20 @@ class Search:
         self.keyword = search_keyword
         self.session = session
         self.owner_memos_public = Memos.objects.filter(made_by=self.account_owner, disclose="public").exclude(tab_name="TRASH")
-        self.owner_memos = Memos.objects.filter(made_by=self.account_owner)
+        self.owner_memos = Memos.objects.filter(made_by=self.account_owner)    
         self.logged_user = ""
         if self.session:            
-            self.logged_user = Tabs(self.session)
+            self.logged_user = Tabs(self.session)          
             if self.keyword[0] == "!":
                 self.keyword = self.keyword[1:]
                 if self.account_owner.user_id != self.session: #for logged user context with ! keyword search
                     self.memos_found = Memos.objects.filter(made_by=self.account_owner, keyword__icontains=self.keyword, disclose="public" ).exclude(tab_name="TRASH")
+                    dms_got = DMs.objects.filter(to=self.logged_user.user)
+                    dms_got_nocheck = DMs.objects.filter(to=self.logged_user.user, if_to_checked="no")
+                    dms_got_noReplyCheck = DMs.objects.filter(to=self.logged_user.user, if_to_reply_checked="no")
+                    dms_sent = DMs.objects.filter(sent_by=self.owner)
+                    dms_sent_noReplyCheck = DMs.objects.filter(sent_by=self.logged_user.user, if_from_reply_checked="no")
+                    dms_notificaion_count = len(dms_got_nocheck) + len(dms_got_noReplyCheck) + len(dms_sent_noReplyCheck)
                     self.context = {
                         'keyword':self.keyword,
                         'memos_found':self.memos_found,
@@ -29,10 +35,22 @@ class Search:
                         'tab03':self.logged_user.tab03,
                         'tab04':self.logged_user.tab04,
                         'tab05':self.logged_user.tab05,
-                        'category':"keyword"
+                        'category':"keyword",
+                        'dms_got':dms_got,
+                        'dms_got_nocheck':dms_got_nocheck,
+                        'dms_got_noReplyCheck':dms_got_noReplyCheck,
+                        'dms_sent':dms_sent,
+                        'dms_sent_noReplyCheck':dms_sent_noReplyCheck,
+                        'dms_notification_count': dms_notificaion_count,
                     }
                 else: #for owner context with ! keyword search
                     self.memos_found = Memos.objects.filter(made_by=self.account_owner, keyword__icontains=self.keyword)
+                    dms_got = DMs.objects.filter(to=self.ownerInfo.owner)
+                    dms_got_nocheck = DMs.objects.filter(to=self.ownerInfo.owner, if_to_checked="no")
+                    dms_got_noReplyCheck = DMs.objects.filter(to=self.ownerInfo.owner, if_to_reply_checked="no")
+                    dms_sent = DMs.objects.filter(sent_by=self.ownerInfo.owner)
+                    dms_sent_noReplyCheck = DMs.objects.filter(sent_by=self.ownerInfo.owner, if_from_reply_checked="no")
+                    dms_notificaion_count = len(dms_got_nocheck) + len(dms_got_noReplyCheck) + len(dms_sent_noReplyCheck)
                     self.context = {
                         'keyword':self.keyword,
                         'memos_found':self.memos_found,
@@ -46,11 +64,23 @@ class Search:
                         'tab03':self.logged_user.tab03,
                         'tab04':self.logged_user.tab04,
                         'tab05':self.logged_user.tab05,
-                        'category':"keyword"
+                        'category':"keyword",
+                        'dms_got':dms_got,
+                        'dms_got_nocheck':dms_got_nocheck,
+                        'dms_got_noReplyCheck':dms_got_noReplyCheck,
+                        'dms_sent':dms_sent,
+                        'dms_sent_noReplyCheck':dms_sent_noReplyCheck,
+                        'dms_notification_count': dms_notificaion_count,
                     }
             else:
                 if self.account_owner.user_id != session: # for logged user context with subject search
                     self.memos_found = Memos.objects.filter(made_by=self.account_owner, subject__icontains=self.keyword, disclose="public" ).exclude(tab_name="TRASH")
+                    dms_got = DMs.objects.filter(to=self.logged_user.user)
+                    dms_got_nocheck = DMs.objects.filter(to=self.logged_user.user, if_to_checked="no")
+                    dms_got_noReplyCheck = DMs.objects.filter(to=self.logged_user.user, if_to_reply_checked="no")
+                    dms_sent = DMs.objects.filter(sent_by=self.owner)
+                    dms_sent_noReplyCheck = DMs.objects.filter(sent_by=self.logged_user.user, if_from_reply_checked="no")
+                    dms_notificaion_count = len(dms_got_nocheck) + len(dms_got_noReplyCheck) + len(dms_sent_noReplyCheck)
                     self.context = {
                         'keyword':self.keyword,
                         'memos_found':self.memos_found,
@@ -64,10 +94,22 @@ class Search:
                         'tab03':self.logged_user.tab03,
                         'tab04':self.logged_user.tab04,
                         'tab05':self.logged_user.tab05,
-                        'category':"subject"
+                        'category':"subject",
+                        'dms_got':dms_got,
+                        'dms_got_nocheck':dms_got_nocheck,
+                        'dms_got_noReplyCheck':dms_got_noReplyCheck,
+                        'dms_sent':dms_sent,
+                        'dms_sent_noReplyCheck':dms_sent_noReplyCheck,
+                        'dms_notification_count': dms_notificaion_count,
                     }
                 else: # owner context with subject search
                     self.memos_found = Memos.objects.filter(made_by=self.account_owner, subject__icontains=self.keyword)
+                    dms_got = DMs.objects.filter(to=self.ownerInfo.owner)
+                    dms_got_nocheck = DMs.objects.filter(to=self.ownerInfo.owner, if_to_checked="no")
+                    dms_got_noReplyCheck = DMs.objects.filter(to=self.ownerInfo.owner, if_to_reply_checked="no")
+                    dms_sent = DMs.objects.filter(sent_by=self.ownerInfo.owner)
+                    dms_sent_noReplyCheck = DMs.objects.filter(sent_by=self.ownerInfo.owner, if_from_reply_checked="no")
+                    dms_notificaion_count = len(dms_got_nocheck) + len(dms_got_noReplyCheck) + len(dms_sent_noReplyCheck)
                     self.context = {
                         'keyword':self.keyword,
                         'memos_found':self.memos_found,
@@ -81,7 +123,13 @@ class Search:
                         'tab03':self.logged_user.tab03,
                         'tab04':self.logged_user.tab04,
                         'tab05':self.logged_user.tab05,
-                        'category':"subject"
+                        'category':"subject",
+                        'dms_got':dms_got,
+                        'dms_got_nocheck':dms_got_nocheck,
+                        'dms_got_noReplyCheck':dms_got_noReplyCheck,
+                        'dms_sent':dms_sent,
+                        'dms_sent_noReplyCheck':dms_sent_noReplyCheck,
+                        'dms_notification_count': dms_notificaion_count,
                     }
         else:
             if self.keyword[0] == "!":

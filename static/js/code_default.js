9755f64c00b3b651a4d6c4596da12c3c488ade1b
Sean Choi <seanchoi@seans-mac-mini.myfiosgateway.com>
2020-11-01 18:00:14 -0500

    viewmemo update

--- static/js/code_default.js
+++ static/js/code_default.js
@@ -1,16 +1,6 @@
 $(document).ready(function() {
     var code = $('.code')[0];
     var mode = $('.language').val();
-    var mixedMode = {
-        name: "htmlmixed",
-        scriptTypes: [{matches: /\/x-handlebars-template|\/x-mustache/i,
-                       mode: null},
-                      {matches: /(text|application)\/(x-)?vb(a|script)/i,
-                       mode: "vbscript"}]
-      };
-    if (mode == "htmlmixed") {
-        mode = mixedMode
-    }
     var editor = CodeMirror.fromTextArea(code, {
         lineNumbers: true,
         mode: mode,
